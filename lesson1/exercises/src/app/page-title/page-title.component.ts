d7b5521b8f982bb651199a287e9bfc509bd9356c
MacStation2 <mac2@KaelasWkstation.hiteccopy.local>
2021-06-28 16:18:44 -0500

    Part3

---
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -0,0 +1,20 @@
+import { stringify } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-title',
+  templateUrl: './page-title.component.html',
+  styleUrls: ['./page-title.component.css']
+})
+export class PageTitleComponent implements OnInit {
+
+  title = "Page Title";
+  
+  constructor() { 
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
